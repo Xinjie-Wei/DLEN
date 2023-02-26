6b5b130e49f47cfeebd9ef97a1dcaf758fd52f33
Xinjie-Wei <90489027+Xinjie-Wei@users.noreply.github.com>
2023-02-26 17:13:03 +0800

    Add files via upload

---
+++ models/losses/loss.py
@@ -0,0 +1,36 @@
+from torch import nn
+from torch.nn import functional as F
+_reduction_modes = ['none', 'mean', 'sum']
+
+
+def l1_loss(pred, target):
+    return F.l1_loss(pred, target, reduction='none')
+
+class L1Loss(nn.Module):
+    """L1 (mean absolute error, MAE) loss.
+
+    Args:
+        loss_weight (float): Loss weight for L1 loss. Default: 1.0.
+        reduction (str): Specifies the reduction to apply to the output.
+            Supported choices are 'none' | 'mean' | 'sum'. Default: 'mean'.
+    """
+
+    def __init__(self, loss_weight=1.0, reduction='mean'):
+        super(L1Loss, self).__init__()
+        if reduction not in ['none', 'mean', 'sum']:
+            raise ValueError(f'Unsupported reduction mode: {reduction}. '
+                             f'Supported ones are: {_reduction_modes}')
+
+        self.loss_weight = loss_weight
+        self.reduction = reduction
+
+    def forward(self, pred, target, weight=None, **kwargs):
+        """
+        Args:
+            pred (Tensor): of shape (N, C, H, W). Predicted tensor.
+            target (Tensor): of shape (N, C, H, W). Ground truth tensor.
+            weight (Tensor, optional): of shape (N, C, H, W). Element-wise
+                weights. Default: None.
+        """
+        return self.loss_weight * l1_loss(
+            pred, target, weight, reduction=self.reduction)
